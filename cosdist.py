2963be4ac24f8d277e37fd99cda8ca63ed1e19c5
CrAZYGreenAlpaca <39609875+CrAZYGreenAlpaca@users.noreply.github.com>
2020-07-16 12:09:11 -0700

    cos dist with norm

--- cosdist.py
+++ cosdist.py
@@ -1,1 +1,27 @@
-def cosine_dist():
+
+
+def cosine_dist(d1, d2):
+    return 1 - (np.dot(d1, d2)) / (norm(d1)*norm(d2))
+"""
+
+    takes image and returns image with face in box/features highlighted
+    
+    Parameters
+    ----------
+    d1 : numpy array
+
+    d2 : numpy array
+
+
+    
+    Returns
+    -------
+    
+    ax, pic
+
+    the ax and pic from the plot
+    
+    """
+
+
+
