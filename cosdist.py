be6dbd345e5ffafcf42733f022063c40aefbc2f8
CrAZYGreenAlpaca <39609875+CrAZYGreenAlpaca@users.noreply.github.com>
2020-07-16 12:11:05 -0700

    normal added

--- cosdist.py
+++ cosdist.py
@@ -1,7 +1,7 @@
 
 
 def cosine_dist(d1, d2):
-    return 1 - (np.dot(d1, d2)) / (norm(d1)*norm(d2))
+    return 1 - (np.dot(d1, d2)) / (np.linalg.norm(d1)* np.linalg.norm(d2))
 """
 
     takes image and returns image with face in box/features highlighted
