6ca4bf98a884c0bcf06e2963c4a7d1d3ea803b47
Austin Yang <austinqyang@gmail.com>
2020-07-17 11:16:20 -0700

    fixed parameters

--- facial_recognition/cosdist_to_adjmatrix.py
+++ facial_recognition/cosdist_to_adjmatrix.py
@@ -1,24 +1,24 @@
 from cosdist import cosine_dist
 import numpy as np
 
-def cosdist_to_adjmatrix(picture_descriptors, cutoff):
+def cosdist_to_adjmatrix(picture_descriptors, threshold):
     """
 
     Parameters
     ----------
-    picture_descriptors : shape(N,) numpy.ndarray
-        numpy array of the descriptors of the pictures
-    cutoff : float value determined by experimentation
+    picture_descriptors : shape(N,) list of descriptors
+        list of the descriptors of the pictures
+    threshold : float value determined by experimentation
         determines whether or not 2 nodes have a connection
         
     Returns
     -------
     adjacency_matrix : shape (N, N) np.ndarray containing weighted cosine distances
     """
-    adjacency_matrix = np.zeros(picture_descriptors.shape[0] ** 2)
-    adjacency_matrix = adjacency_matrix.reshape(picture_descriptors.shape[0], picture_descriptors.shape[0])
+    adjacency_matrix = np.zeros(len(picture_descriptors) ** 2)
+    adjacency_matrix = adjacency_matrix.reshape(len(picture_descriptors), len(picture_descriptors))
     for i in range(adjacency_matrix.shape[0]):
         for j in range(adjacency_matrix.shape[1]):
-            if cosine_dist(picture_descriptors[i], picture_descriptors[j]) < cutoff:
-                adjacency_matrix[i][j] = 1/(cosine_dist(picture_descriptors[i], picture_descriptors[j]) ** 2)
+            if cosdist.cosine_dist(picture_descriptors[i], picture_descriptors[j]) < threshold:
+                adjacency_matrix[i][j] = 1/(cosdist.cosine_dist(picture_descriptors[i], picture_descriptors[j]) ** 2)
     return adjacency_matrix
