1a00af88e3abd72909d31a7472b074546209adce
Olivia Ma <olivia.ma@myxing.net>
2020-07-17 11:01:31 -0400

    wrote how-to, __add__

---
+++ __init__.py
@@ -0,0 +1,17 @@
+import ProfileDatabase
+
+"""
+WELCOME TO OUR FACIAL RECOGNITION PACKAGE
+-----------------------------------------
+This package will take in photos you provide it and recognize the faces in those photos
+
+The Profile class stores the information related to a face; their name and their face descriptors
+ProfileDatabase matches a name to the profile. It has capabilities to add and remove profiles or pictures
+
+HOW TO USE
+----------
+Run populate_database to load the image database
+ProfileDatabase class has functions to add/remove profiles, add images from camera or file, match image to profile
+
+"""
+__all__ = ["DisplayImage", "populate_database", "ProfileDatabase", "whispers"]
